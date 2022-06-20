fbf29e0d639c0352d3990d2f535926a2cd7a8b95
safa maks <maksimka-maksimka30@mail.ru>
2022-06-20 10:16:15 +0300

    Добавьте файлы проекта.

---
+++ MyPages/AddRedacServicePage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Secvice.MyPages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddRedacServicePage.xaml
+    /// </summary>
+    public partial class AddRedacServicePage : Page
+    {
+        private Model.Service _currentService = new Model.Service();
+        public AddRedacServicePage(Model.Service selectedService)
+        {
+            InitializeComponent();
+            if(selectedService != null)
+            {
+                _currentService = selectedService;
+            }
+            DataContext = _currentService;
+        }
+
+        private void BnBack_Click(object sender, RoutedEventArgs e)
+        {
+            MyClass.Manager.navFrame.GoBack();
+        }
+
+        private void BnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentService.Title))
+                errors.AppendLine("Title");
+            if (_currentService.Cost <= 0)
+                errors.AppendLine("Cost");
+            if (_currentService.DurationInSeconds <= 0)
+                errors.AppendLine("Time");
+         
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            try
+            {
+                if (_currentService.ID == 0)
+                    MyClass.ConnectDB.atttrEntities.Service.Add(_currentService);
+                
+                MyClass.ConnectDB.atttrEntities.SaveChanges();
+                MessageBox.Show("Save succsess");
+                MyClass.Manager.navFrame.GoBack();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ MyPages/ServicePage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Secvice.MyPages
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicePage.xaml
+    /// </summary>
+    public partial class ServicePage : Page
+    {
+        public ServicePage()
+        {
+            InitializeComponent();
+            var CR = MyClass.ConnectDB.atttrEntities.Service.ToList();
+            LV.ItemsSource = CR;
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyClass.Manager.navFrame.Navigate(new MyPages.AddRedacServicePage(null));
+        }
+
+        private void BtRedac_Click(object sender, RoutedEventArgs e)
+        {
+            MyClass.Manager.navFrame.Navigate(new MyPages.AddRedacServicePage((sender as Button).DataContext as Model.Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            MyClass.ConnectDB.atttrEntities.ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+            LV.ItemsSource = MyClass.ConnectDB.atttrEntities.Service.ToList();
+        }
+
+        private void BtYdal_Click(object sender, RoutedEventArgs e)
+        {
+            var service = ((sender as Button).DataContext as Model.Service);
+            if (MessageBox.Show($"You sure delete {((sender as Button).DataContext as Model.Service).Title}","Caution", 
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    MyClass.ConnectDB.atttrEntities.Service.Remove(service);
+                    MyClass.ConnectDB.atttrEntities.SaveChanges();
+                    MessageBox.Show("Data delete");
+                    MyClass.ConnectDB.atttrEntities.ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                    LV.ItemsSource = MyClass.ConnectDB.atttrEntities.Service.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}
